e7307fb19ecc84821c395225bf86aea3491be370
Jasmine <jasmine.kim.18@gmail.com>
2019-11-25 16:39:15 -0800

    Hello page fades in using React TransitionGroup add-on

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import Hello from '../Hello'
 import "./styles.scss";
+import { CSSTransitionGroup } from 'react-transition-group' // ES6
+
 
 function Landing() {
+  // read up on using CSS transitions here:
+  //https://github.com/reactjs/react-transition-group/tree/v1-stable
+
   return (
     <div>
-      <Hello/>
+      <CSSTransitionGroup
+      transitionName="fadein"
+      transitionAppear={true}
+      transitionAppearTimeout={500}
+      transitionEnter={false}
+      transitionLeave={false}>
+        <Hello/>
+      </CSSTransitionGroup>
     </div>
 
   );
